4d2054ee8451768ad125d91622feac847e94e049
MVDebian-Clase <alberto.sanchez@enti.cat>
2024-11-15 10:22:07 +0100

    servidor y cliente funcionando

--- client.sh
+++ client.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-
-clear
-#ENVIAR CABEZERA
-IPV4=localhost
+clear 
+IPV4=localhost 
 PUERTO=2022
 BUCLE=1
-
 while [ $BUCLE == 1 ]; do
+clear
+echo "----------------------------------------------------------------------------"
+echo "envia la cabecera DMAM FILE_NAME seguido del nombre del archivo para completar la conexión, envia otra para interrumpirla"
 
-echo "----------------------------------------------------------------------------------"
-echo "envia la cabecera DMAM para completar la conexión, envía otra para interrumpirla"
 read CABECERA
 echo "enviando cabecera $CABECERA a $IPV4 en el puerto $PUERTO"
 sleep 1
 echo $CABECERA | ncat $IPV4 $PUERTO
-echo "----------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------"
 echo "recibiendo cabecera del servidor"
 CABECERA_SERVER=`ncat -l localhost $PUERTO`
 ERROR_SERVER=`ncat -l localhost $PUERTO`
-if [ "$CABECERA_SERVER" == "OK_HEADER" ]; then
-
-	echo "la cabecera recibida es: $CABECERA_SERVER, CONEXIÓN COMPLETADA"
-else 
-
-	echo "la cabecera recibida es: $CABECERA_SERVER, CONEXIÓN INTERRUMPIDA"
-	echo "$ERROR_SERVER"
-	BUCLE=0
-fi
-echo "-----------------------------------------------------------------------------------"
+ 	if [ "$CABECERA_SERVER" == "OK HEADER" ]; then
+		 echo "la cabecera recibida es $CABECERA_SERVER, CONEXIÓN COMPLETADA"
+	else
+ 		 echo "la cabecera recibida es $CABECERA_SERVER, CONEXIÓN INTERRUMPIDA"
+		 echo "$ERROR_SERVER"
+		 BUCLE=0
+	fi
+echo "----------------------------------------------------------------------------"
 sleep 3
 done
--- server.sh
+++ server.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+
 clear
 PUERTO=2022
-CABECERA_REAL="DMAM"
-echo "ESCUCHANDO EL PUERTO $PUERTO"
+CABECERA_REAL="DMAM FILE_NAME"
+echo "ESCUCHANDO EL PUERTO $PUERTO" 
 BUCLE_SERVER=1
 
 while [ $BUCLE_SERVER == 1 ]; do
 
-CABECERA=`ncat -l localhost $PUERTO`
+	CABECERA=`ncat -l localhost $PUERTO`
+	CABECERA_FILENAME=$( echo "$CABECERA" | cut -b 1-14 )
+	CABECERA_NOMBRE=$( echo "$CABECERA" | cut -b 16- )
 
-if [ "$CABECERA" == "$CABECERA_REAL" ]; then
+	if [ "$CABECERA_FILENAME" == "$CABECERA_REAL" ]; then
 
-	echo "se ha recibido la cabecera correcta"
-	echo "OK_HEADER" | ncat localhost $PUERTO
-	echo "ERROR_0: Cabecera correcta" | ncat localhost $PUERTO
-else 
-	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | ncat localhost $PUERTO
-	echo "ERROR_1: Cabecera incorrecta" | ncat localhost $PUERTO
-	BUCLE_SERVER=0
-fi
+		 echo "se na recibido la cabecera correcta"
+		 echo "OK HEADER" | ncat localhost $PUERTO
+		 echo "SE HA RECIBIDO EL ARCHIVO $CABECERA_NOMBRE"
+		 echo "ERROR_0: Cabecera correcta" | ncat localhost $PUERTO
+	else
+		echo "ERROR_1: Cabecera incorrecta"
+		echo "KO_HEADER" | ncat localhost $PUERTO
+		echo "	RECIBIDO: $CABECERA, CABECERA RECIBIDA: $CABECERA_FILENAME | CABECERA ESPERADA: $CABECERA_REAL"
+		echo "ERROR_1: Cabecera incorrecta" | ncat localhost $PUERTO
+		if [ "$CABECERA" == "101" ]; then
+ 		BUCLE_SERVER=0
+	fi
+	fi
 done
